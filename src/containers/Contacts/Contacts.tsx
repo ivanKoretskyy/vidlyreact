7a773da1472a50cc448644b7cca18e045a594902
Ivan Koretskyy <ivankoretskyy@gmail.com>
2019-02-03 11:54:22 +0200

    Add google map to contact page

--- src/containers/Contacts/Contacts.tsx
+++ src/containers/Contacts/Contacts.tsx
@@ -11,11 +11,17 @@ export interface ContactsState {
  
 class Contacts extends React.Component<ContactsProps, ContactsState> {
  // state = { :  }
+componentDidMount() {
+  const map = new (window as any).google.maps.Map(document.getElementById('map'), {
+    center: { lat: 41.0082, lng: 28.9784 },
+    zoom: 8
+  });
+}
+
+
   render() { 
     return ( 
-      <div>
-        Contacts
-      </div>
+      <div style={{ width: 1500, height: 1500 }} id="map" />
      );
   }
 }
